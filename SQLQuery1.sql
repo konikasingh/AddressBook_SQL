c4260f2e2a8f4417c2e194beacd1cece023f958a
konika <konikasingh1996@gmail.com>
2021-12-08 09:09:27 +0530

    create table of addressbook

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,18 @@
 --UC1-Ability to create address book database
 create database address_bookk;
 use address_bookk;
+
+---UC2 : Ability create a address book table database
+CREATE TABLE address_book_System(
+id int IDENTITY(1,1) PRIMARY KEY,
+First_Name varchar(50),
+Last_Name varchar(50),
+Address varchar(200),
+City varchar(50),
+State varchar(50),
+Zip int,
+Phone_Number bigint,
+Email_Id varchar(200)
+);
+select * from address_book_System
+
